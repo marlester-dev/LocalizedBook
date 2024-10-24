5e3f513f33f6967f7590c4229e24c048cdc22f5e
Marlester <77095872+marlester-dev@users.noreply.github.com>
2024-10-24 19:18:58 +0200

    add missing property

---
+++ common/src/main/java/me/marlester/localizedbook/util/Environment.java
@@ -0,0 +1,20 @@
+package me.marlester.localizedbook.util;
+
+import java.nio.file.Path;
+
+public final class Environment {
+  private Environment() {
+  }
+
+  public static boolean isClient() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static Path getConfigPath() {
+    throw new UnsupportedOperationException();
+  }
+
+  public static boolean isModLoaded(String id) {
+    throw new UnsupportedOperationException();
+  }
+}
